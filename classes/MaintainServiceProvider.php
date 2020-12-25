cce5005f8b3156c4090d382a517f96f305170475
王正东 <wangzhengdong@ecmoban.com>
2020-12-25 16:44:15 +0800

    加载服务

--- classes/MaintainServiceProvider.php
+++ classes/MaintainServiceProvider.php
@@ -23,6 +23,8 @@ class MaintainServiceProvider extends  AppParentServiceProvider
         RC_Service::addService('admin_purview', 'maintain', 'Ecjia\App\Maintain\Services\AdminPurviewService');
         RC_Service::addService('tool_menu', 'maintain', 'Ecjia\App\Maintain\Services\ToolMenuService');
         RC_Service::addService('plugin_menu', 'maintain', 'Ecjia\App\Maintain\Services\PluginMenuService');
+        RC_Service::addService('plugin_install', 'maintain', 'Ecjia\App\Maintain\Services\PluginInstallService');
+        RC_Service::addService('plugin_uninstall', 'maintain', 'Ecjia\App\Maintain\Services\PluginUninstallService');
     }
     
 }
